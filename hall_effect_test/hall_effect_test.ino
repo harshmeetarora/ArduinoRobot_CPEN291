4bd202732b9ffc465a7efbdbbbb1e4ef2334d99c
lausi119 <laurenz.schmielau@brentwood.ca>
2017-03-01 17:04:20 -0800

    Update hall_effect_test.ino

--- hall_effect_test/hall_effect_test.ino
+++ hall_effect_test/hall_effect_test.ino
@@ -1,6 +1,8 @@
+#define hallEffect1 2
+
 void setup() {
    Serial.begin (9600);
-   pinMode(2, INPUT_PULLUP);
+   pinMode(hallEffect1, INPUT_PULLUP);
 
 }
 
@@ -13,16 +15,13 @@ void readHallSpeed(){
   float halfRotationTime;
 
   halfRotationTime = millis();
-  while(digitalRead != 1);
+  while(digitalRead(hallEffect1) != 1);
   halfRotationTime = millis();
-  while(digitalRead(2) == 1);
-  while(digitalRead(2) == 0);
+  while(digitalRead(hallEffect1) == 1);
+  while(digitalRead(hallEffect1) == 0);
   halfRotationTime = (millis() - halfRotationTime) / 1000; //devided by 1000 to translate millis into seconds
 
-  return (0.5 * (PI * 6.5) ) / halfRotationTime; 
-  
-
-  
+  return (0.5 * (PI * 6.5) ) / halfRotationTime;
   
 }
 
