1ae62158b0fa2af9e323769a2c261adea97fa3ce
lausi119 <laurenz.schmielau@brentwood.ca>
2017-03-06 17:54:23 -0800

    updated

--- Hall_effect_2.0/Hall_effect_2.0.ino
+++ Hall_effect_2.0/Hall_effect_2.0.ino
@@ -10,6 +10,7 @@ double tireSpeedLeft;
 double tireSpeedRight;
 double controlSpeedLeft;
 double controlSpeedRight;
+double desiredSpeed = 15;
 
 unsigned long lastInterruptRight = 0;
 unsigned long lastInterruptLeft = 0;
@@ -29,54 +30,45 @@ void setup() {
    controlSpeedRight = 0;
 
    
-//  digitalWrite(M1,LOW);   
-//   digitalWrite(M2, LOW);       
-//   analogWrite(E1, 140);   //PWM Speed Control
-//   analogWrite(E2, 0);   //PWM Speed Control
+  digitalWrite(M1,LOW);   
+   digitalWrite(M2, LOW);       
+   analogWrite(E1, 255);   //PWM Speed Control
+   analogWrite(E2, 100);   //PWM Speed Control
 }
 
 void loop() {  
 
   checkLeftHallEffectSensor();
-
   
 
 }
 
 
 void checkLeftHallEffectSensor(){
-  int i;
-  i = analogRead(A0);
-//  Serial.println(i);
-  if ( ((millis() - lastInterruptLeft) >  1000)  && (i < 100) ){
+  
+  if ( ((millis() - lastInterruptLeft) >  200)  && (analogRead(A0) < 100) ){  
+    
     if ( firstReadingLeft){
-      halfRotationTimeLeft = millis();
+      lastInterruptLeft = millis();
+      halfRotationTimeLeft = lastInterruptLeft;
       firstReadingLeft = 0;
-      Serial.println("first detect");
     } else {
       lastInterruptLeft = millis();
       tireSpeedLeft = 0.5 * PI * 6.5 / ((millis() - halfRotationTimeLeft) / 1000) ; //devided by 1000 to translate millis into seconds
       firstReadingLeft = 1;
-      Serial.println("second detect");
-      Serial.println(tireSpeedLeft);
-
-      
-      //updateLeftTireSpeed(desiredSpeed);
-      // possibly update speed here
-      
+      updateLeftTireSpeed();
     }
-
-    Serial.println(" once or twice");
+    
+  } else if ( (millis() - lastInterruptLeft) > 1000){
+    updateLeftTireSpeed();
   }
-
-  
-  
+   
 }
 
 void checkRightHallEffectSensor(){
-  int i;
-  i = analogRead(A1);
-  if ( ((millis() - lastInterruptRight) >  1000)  && (i < 100) ){
+  
+  if ( ((millis() - lastInterruptRight) >  1000)  && (analogRead(A5) < 100) ){
+    
     if ( firstReadingRight){
       halfRotationTimeRight = millis();
       firstReadingRight = 0;
@@ -84,34 +76,27 @@ void checkRightHallEffectSensor(){
       lastInterruptRight = millis();
       tireSpeedRight = 0.5 * PI * 6.5 / ((millis() - halfRotationTimeRight) / 1000) ; //devided by 1000 to translate millis into seconds
       firstReadingRight = 1;
-      //updateRightTireSpeed(desiredSpeed);
-      // possibly update speed here
-      
+      updateRightTireSpeed(); 
     }
+    
+  } else if ( (millis() - lastInterruptRight) > 1000){
+     updateRightTireSpeed();
   }
   
 }
 
 
-void updateLeftTireSpeed(double desiredSpeed){
+void updateLeftTireSpeed(){
     double change;
     double PMWDriveSignal;
     
-  
     change = (desiredSpeed - tireSpeedLeft) ;
     controlSpeedLeft = max(controlSpeedLeft + change, 0);
     if (controlSpeedLeft > 65) {
       controlSpeedLeft = 65;
     }
-        PMWDriveSignal = map(controlSpeedLeft , 0, 65, 0, 255);  
-    Serial.print("desiredSpeed : "); Serial.println(desiredSpeed);
-    Serial.print("tireSpeedLeft : "); Serial.println(tireSpeedLeft);
-    Serial.print("change : "); Serial.println(change);
-    Serial.print("controlSpeedLeft : "); Serial.println(controlSpeedLeft);
-    Serial.print("PMW signal : "); Serial.println(PMWDriveSignal);
-    Serial.println("-----------------------------------------------");
-     
-    digitalWrite(M2, HIGH);       
+    PMWDriveSignal = map(controlSpeedLeft , 0, 65, 0, 255);      
+    digitalWrite(M2, LOW);       
     analogWrite(E2, PMWDriveSignal );   //PWM Speed Control
     if (PMWDriveSignal < 20){    // update tire speed to 0 if DriveSignal is too small to make the tire turn 
       tireSpeedLeft = 0;  
@@ -119,19 +104,16 @@ void updateLeftTireSpeed(double desiredSpeed){
   
 }
 
-void updateRightTireSpeed(double desiredSpeed){
+void updateRightTireSpeed(){
     double change;
     double PMWDriveSignal;
     
     change = desiredSpeed - tireSpeedRight;
-    
     controlSpeedRight = max(controlSpeedRight + change, 0);
     if (controlSpeedRight > 65) {
       controlSpeedRight = 65;
     }
-
     PMWDriveSignal = map(controlSpeedRight , 0, 65, 0, 255);
-    
     digitalWrite(M1, HIGH);       
     analogWrite(E1, PMWDriveSignal);   //PWM Speed Control  
     if (PMWDriveSignal < 20){ // update tire speed to 0 if DriveSignal is too small to make the tire turn 
@@ -139,3 +121,12 @@ void updateRightTireSpeed(double desiredSpeed){
     }
   
 }
+
+/*
+    Serial.print("desiredSpeed : "); Serial.println(desiredSpeed);
+    Serial.print("tireSpeedLeft : "); Serial.println(tireSpeedLeft);
+    Serial.print("change : "); Serial.println(change);
+    Serial.print("controlSpeedLeft : "); Serial.println(controlSpeedLeft);
+    Serial.print("PMW signal : "); Serial.println(PMWDriveSignal);
+    Serial.println("-----------------------------------------------");
+    */
